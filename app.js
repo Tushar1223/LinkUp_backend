447dd122b3ce2bab0af9b51b7c43455ae17568b4
Tushar Sharma <tushar007king@gmail.com>
2024-08-10 22:36:48 +0530

    app.js editing

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
-const path = require("path");
+// const path = require("path");
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config({ path: "backend/config/config.env" });
+  require("dotenv").config({ path: "config/config.env" });
 }
 
 // Using Middlewares
@@ -19,10 +19,10 @@ const user = require("./routes/user");
 app.use("/api/v1", post);
 app.use("/api/v1", user);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 
 module.exports = app;
